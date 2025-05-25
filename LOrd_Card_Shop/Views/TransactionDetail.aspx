5c04fc33abc4b7788148c373099546557559f429
Darrel Gautama <darrel.gautama@binus.edu>
2025-05-25 14:04:06 +0700

    Drel (Temp)

---
+++ LOrd_Card_Shop/Views/TransactionDetail.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/Navbar.Master" AutoEventWireup="true" CodeBehind="TransactionDetail.aspx.cs" Inherits="LOrd_Card_Shop.Views.TransactionDetail" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ LOrd_Card_Shop/Views/TransactionHistory.aspx.cs
@@ -0,0 +1,28 @@
+﻿using LOrd_Card_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LOrd_Card_Shop.Views
+{
+    public partial class TransactionHistory : System.Web.UI.Page
+    {
+        Database1Entities db = new Database1Entities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<TransactionHeader> list = db.TransactionHeader.ToList();
+
+            GridView1.DataSource = list;
+            GridView1.DataBind();
+        }
+        
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            String id = GridView1.DataKeys[e.NewEditIndex].Value.ToString();
+            Response.Redirect("~/Views/TransactionDetail.aspx?id=" + id);
+        }
+    }
+}
